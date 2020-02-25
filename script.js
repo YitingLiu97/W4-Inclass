3e44578cfbdaca27378a9a4ac1f243dc8cd7870a
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2020-02-24 20:16:17 -0500

    add .gitignore, package.json.

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ const fs = require ('fs');
 //give tools file system's path to you 
 // e.g.: Desktop/xx/xx/xx
 const path = require('path');
+const metalBandGenerator = require('metal-band-generator');
+console.log(metalBandGenerator.getBandName());
 
 // sync = synchronization
 //
 const content =fs.readFileSync('pizzaTopping.json');
 
 let pizzaToppings=JSON.parse(content);
-console.log(pizzaToppings);
+// console.log(pizzaToppings);
 
 
 // let name ="Cassie";
